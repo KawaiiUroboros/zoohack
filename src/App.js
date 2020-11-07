1e4c47092cf0b77034933b085b55ed25842563fb
Kolbak <kushaevmarat@mail.ru>
2020-11-07 15:27:44 +0300

    dfgjhgk

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+
+// import map from "./assets/map.png";
+import fire from "./assets/fire.svg";
+
+const styles = {
+  map: {
+    position: "absolute",
+    width: "100vw",
+    height: "100vh",
+  },
+  img: {
+    width: "100%",
+    height: "100%",
+  },
+  panel: {
+    position: "absolute",
+    left: "calc(50% - 40px)",
+    width: "80px",
+    height: "80px",
+    background: "#FF565E",
+    borderRadius: "50px 50px 0px 0px",
+    bottom: 0,
+  },
+  btn: {
+    margin: "12.5% 0 0 12.5%",
+    outline: "none",
+    width: "60px",
+    height: "60px",
+    borderRadius: "100%",
+    background: "#20212A",
+    border: "none",
+  },
+};
+
+function App() {
+  const TOKEN = "AIzaSyDgLtLfp5_qOZnj4Y2Uc7oKMf3notPmokI"
+  let str = `https://www.google.com/maps/embed/v1/place?key=${TOKEN}
+  &q=Space+Needle,Seattle+WA`
+
+  return (
+    <div className="container">
+      <div className="map" style={styles.map}>
+        {/* <img src={map} alt="map" style={styles.img}></img> */}
+        {/* <iframe
+          width="600"
+          height="450"
+          frameborder="0"
+          style="border:0"
+          src={str}
+          allowfullscreen
+        ></iframe> */}
+      </div>
+      <div className="panel" style={styles.panel}>
+        <button style={styles.btn}>
+          <img src={fire} alt="alert button"></img>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
