9a97376de3de7e8e6d88be7d7cc9df1aaf5a1f7b
Kolbak <kushaevmarat@mail.ru>
2020-11-08 13:37:50 +0300

    sendind pic

--- src/Map.js
+++ src/Map.js
@@ -11,6 +11,23 @@ function WarnMap() {
   function disappearWarnPage(e) {
     document.querySelector(".warn-page").style.height = "0vh";
   }
+  async function onFileInput(e) {
+    let formData = new FormData();
+    let file = e.target.files[0];
+    formData.append("image", file);
+    try {
+      let r = await fetch("https://poach-server.herokuapp.com/", {
+        method: "POST",
+        body: formData,
+      });
+      let json = await r.json();
+      let possibility = json.possibility;
+      document.querySelector('.possibility').innerText = possibility;
+      console.log("HTTP response code:", json);
+    } catch (e) {
+      console.log("Huston we have problem...:", e);
+    }
+  }
   return (
     <div className="container">
       <div id="map" className="map"></div>
@@ -21,19 +38,24 @@ function WarnMap() {
       </div>
       <div className="warn-page">
         <button onClick={disappearWarnPage} className="btn-back">
-          К карте
+          To the map
         </button>
         <form>
           <label className="file-label">
-            <span className="file-span">Выберите файл</span>
-            <input type="file" className="file"></input>
+            <span className="file-span">Choose file</span>
+            <input type="file" className="file" onInput={onFileInput}></input>
           </label>
           <label className="text-label">
-            <p className="p-address">Адрес:</p>
+            <p className="p-address">Address:</p>
             <input type="text" className="text-input"></input>
           </label>
+          <p className="p-possibility">
+            Possibility <br />
+            <span className="possibility"></span>
+            <br />
+          </p>
           <button className="btn-send" type="submit">
-            Отправить
+            Send
           </button>
         </form>
       </div>
